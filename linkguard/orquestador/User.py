0c416fc5dcf9fa627001516bd62e4cbf0eee135e
NataliaPerez08 <nataliaapr08@gmail.com>
2025-01-09 22:49:59 -0600

    Copia login

--- linkguard/orquestador/User.py
+++ linkguard/orquestador/User.py
@@ -1,42 +1,34 @@
-class User:
-    def __init__(self, name, email, password):
+from flask_login import UserMixin
+
+from linkguard.orquestador.db import get_db
+
+class User(UserMixin):
+    def __init__(self, id_, name, email, profile_pic):
+        self.id = id_
         self.name = name
         self.email = email
-        self.password = password
-        # Diccionario de redes privadas {id: RedPrivada}
-        self.private_networks = dict()
+        self.profile_pic = profile_pic
 
-        # Contador de redes privadas
-        self.private_network_counter = 0
-
-    def __str__(self):
-        return "Name: " + self.name
-
-    def add_private_network(self, private_network):
-        self.private_networks.append(private_network)
-
-    def remove_private_network(self, private_network):
-        self.private_networks.remove(private_network)
-
-    def get_private_networks(self):
-        return self.private_networks
-
-    def get_private_network(self, private_network_id):
-        for private_network in self.private_networks:
-            if private_network.id == private_network_id:
-                return private_network
-        return None
-    
-    def get_private_network_by_id(self, private_network_id):
-        print("Buscando red privada...")
-        try:
-            print(self.private_networks[private_network_id])
-            return self.private_networks[private_network_id]
-        except KeyError:
+    @staticmethod
+    def get(user_id):
+        db = get_db()
+        user = db.execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+        if not user:
             return None
 
-    def add_private_network(self, private_network):
-        self.private_networks.append(private_network)
+        user = User(
+            id_=user[0], name=user[1], email=user[2], profile_pic=user[3]
+        )
+        return user
 
-    def get_num_private_networks(self):
-        return len(self.private_networks)
+    @staticmethod
+    def create(id_, name, email, profile_pic):
+        db = get_db()
+        db.execute(
+            "INSERT INTO user (id, name, email, profile_pic) "
+            "VALUES (?, ?, ?, ?)",
+            (id_, name, email, profile_pic),
+        )
+        db.commit()
